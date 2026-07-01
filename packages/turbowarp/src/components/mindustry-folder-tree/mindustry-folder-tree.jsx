4a4c83e488d05f37293dbadf6e2c8fac8f28aa3d
zsd2024 <zsd_2024@outlook.com>
2026-07-01 21:33:57 +0800

    fix: 第2轮 替换右键菜单为 TW ContextMenu 组件
    - asset-cards: remove custom context menu, use react-contextmenu + TW ContextMenu/DangerousMenuItem
    - folder-tree: same pattern, with ContextMenuTrigger wrapping each folder
    - Remove unused custom context menu CSS from both files
    - Folder root (isRoot) disable context menu trigger

--- packages/turbowarp/src/components/mindustry-folder-tree/mindustry-folder-tree.jsx
+++ packages/turbowarp/src/components/mindustry-folder-tree/mindustry-folder-tree.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {ContextMenuTrigger} from 'react-contextmenu';
+import {ContextMenu, MenuItem, DangerousMenuItem} from '../context-menu/context-menu.jsx';
 import styles from './mindustry-folder-tree.css';
 
 class FolderTree extends React.Component {
@@ -9,24 +11,8 @@ class FolderTree extends React.Component {
       collapsed: new Set(),
       renamingId: null,
       renameValue: '',
-      contextMenu: null,
     };
-    this._ctxEl = null;
     this.treeRef = React.createRef();
-  }
-
-  componentDidMount() {
-    document.addEventListener('click', this.handleDocClick);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('click', this.handleDocClick);
-  }
-
-  handleDocClick = (e) => {
-    if (this.state.contextMenu && this._ctxEl && !this._ctxEl.contains(e.target)) {
-      this.setState({contextMenu: null});
-    }
   }
 
   toggleCollapse(id) {
@@ -44,16 +30,8 @@ class FolderTree extends React.Component {
     e.stopPropagation();
   }
 
-  handleContextMenu(id, e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.setState({
-      contextMenu: {folderId: id, x: e.clientX, y: e.clientY, ref: null},
-    });
-  }
-
   startRename(id, currentName) {
-    this.setState({renamingId: id, renameValue: currentName, contextMenu: null});
+    this.setState({renamingId: id, renameValue: currentName});
   }
 
   confirmRename() {
@@ -69,7 +47,7 @@ class FolderTree extends React.Component {
     if (e.key === 'Escape') this.setState({renamingId: null, renameValue: ''});
   }
 
-  renderFolder(folder, depth) {
+  renderFolder(folder) {
     const {assets, selectedFolderId} = this.props;
     const isRoot = !folder.parentId;
     const isCollapsed = this.state.collapsed.has(folder.id);
@@ -86,10 +64,13 @@ class FolderTree extends React.Component {
 
     return (
       <div key={folder.id} className={styles.folder}>
-        <div
-          className={`${styles.folderHeader} ${isActive ? styles.folderActive : ''}`}
-          onClick={(e) => this.handleClick(folder.id, e)}
-          onContextMenu={(e) => this.handleContextMenu(folder.id, e)}
+        <ContextMenuTrigger
+          id={`folder-ctx-${folder.id}`}
+          disable={isRoot}
+          attributes={{
+            className: `${styles.folderHeader} ${isActive ? styles.folderActive : ''}`,
+            onClick: (e) => this.handleClick(folder.id, e),
+          }}
         >
           <span className={styles.folderArrow}>
             {children.length > 0 ? (isCollapsed ? '▶' : '▼') : ''}
@@ -113,11 +94,21 @@ class FolderTree extends React.Component {
           {!isRenaming && childCount > 0 && (
             <span className={styles.folderCount}>{childCount}</span>
           )}
-        </div>
+        </ContextMenuTrigger>
         {!isCollapsed && children.length > 0 && (
           <div className={styles.children}>
-            {children.map(child => this.renderFolder(child, depth + 1))}
+            {children.map(child => this.renderFolder(child))}
           </div>
+        )}
+        {!isRoot && (
+          <ContextMenu id={`folder-ctx-${folder.id}`}>
+            <MenuItem onClick={() => this.startRename(folder.id, folder.name)}>
+              重命名
+            </MenuItem>
+            <DangerousMenuItem onClick={() => this.props.onDeleteFolder(folder.id)}>
+              删除
+            </DangerousMenuItem>
+          </ContextMenu>
         )}
       </div>
     );
@@ -139,44 +130,13 @@ class FolderTree extends React.Component {
             <span className={styles.folderName}>全部</span>
             <span className={styles.folderCount}>{this.props.assets.length}</span>
           </div>
-          {rootFolders.map(f => this.renderFolder(f, 0))}
+          {rootFolders.map(f => this.renderFolder(f))}
         </div>
         <div className={styles.treeFooter}>
           <button className={styles.addFolderBtn} onClick={this.props.onAddFolder}>
             + 新建文件夹
           </button>
         </div>
-
-        {/* context menu */}
-        {this.state.contextMenu && (
-          <div
-            className={styles.contextMenu}
-            style={{left: this.state.contextMenu.x, top: this.state.contextMenu.y}}
-            ref={(el) => {this._ctxEl = el;}}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div
-              className={styles.contextMenuItem}
-              onClick={() => {
-                const f = this.props.folders.find(
-                  f2 => f2.id === this.state.contextMenu.folderId
-                );
-                this.startRename(this.state.contextMenu.folderId, f ? f.name : '');
-              }}
-            >
-              重命名
-            </div>
-            <div
-              className={styles.contextMenuDanger}
-              onClick={() => {
-                this.props.onDeleteFolder(this.state.contextMenu.folderId);
-                this.setState({contextMenu: null});
-              }}
-            >
-              删除
-            </div>
-          </div>
-        )}
       </div>
     );
   }
